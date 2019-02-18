40390490d6deb08330a9ed8f4470a6f0ed0895a1
yusihong1 <yucaihong@jd.com>
2019-02-18 14:19:28 +0800

    :star: 项目初始化 增加bottomBar

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,70 @@
+//google风格
+import 'package:flutter/material.dart';
+//IOS风格
+import 'package:flutter/cupertino.dart';
+
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
+
+class IndexPage extends StatefulWidget {
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  //声明一个list 放我们底部菜单栏的图标和文字
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心'),
+    )
+  ];
+
+  //4个页面的数组
+  final List tabBodies = [HomePage(), CategoryPage(), CartPage(), MemberPage()];
+  //当前选择的项和页面
+  int currentIndex = 0;
+  var currentPage;
+
+  //初始化页面  类似于react的constructor
+  @override
+  void initState() {
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        //当前选择的项
+        currentIndex: currentIndex,
+        //bottombar的内容项
+        items: bottomTabs,
+        //点击bottombar的事件
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
+      ),
+      body: currentPage,
+    );
+  }
+}
