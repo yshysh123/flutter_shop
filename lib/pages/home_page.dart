f4545e874238121fbe352481b6c98502e1a5b0bb
yusihong1 <yucaihong@jd.com>
2019-02-22 15:00:17 +0800

    :star: 增加dio接口 动态组件

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController typeController = TextEditingController();
+  String showText = '欢迎您来到美好人间高级会所';
+
   @override
   Widget build(BuildContext context) {
-    getHttp();
-    return Scaffold(
-      body: Center(
-        child: Text('商城首页'),
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('美好人间'),
+        ),
+        //防止内容越界
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: typeController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: '美女类型',
+                    helperText: '请输入你喜欢的类型',
+                  ),
+                  autofocus: false,
+                ),
+                RaisedButton(
+                  onPressed: _choiceAction,
+                  child: Text('选择完毕'),
+                ),
+                Text(
+                  showText,
+                  //兼容性处理，超出1行展示...
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
-  void getHttp() async {
+  void _choiceAction() {
+    print('开始选择类型');
+    if (typeController.text.toString() == '') {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('请输入文本'),
+              ));
+    } else {
+      getHttp(typeController.text.toString()).then((val) {
+        setState(() {
+          showText = val['data']['name'].toString();
+        });
+      });
+    }
+  }
+
+  //Future作用就是可以在回调使用then方法
+  Future getHttp(String TypeText) async {
     try {
       Response response;
+      var data = {'name': TypeText};
       response = await Dio().get(
-        'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸美女',
+        'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',
+        queryParameters: data,
       );
-      return print(response);
+      return response.data;
     } catch (e) {
       return print(e);
     }
