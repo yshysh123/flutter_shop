7e6ba0d15d1e9ef47bcbe13c59e1d915d2fde72d
yusihong1 <yucaihong@jd.com>
2019-02-25 10:45:36 +0800

    :star: 1.增加接口封装 2.增加homePage接口数据获取

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,82 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
+  final Widget child;
+
+  HomePage({Key key, this.child}) : super(key: key);
+
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎您来到美好人间高级会所';
+  String homePageContent = '正在获取数据';
+
+  @override
+  void initState() {
+    getHomePageContent().then((val) {
+      setState(() {
+        homePageContent = val.toString();
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('美好人间'),
-        ),
-        //防止内容越界
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  controller: typeController,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0),
-                    labelText: '美女类型',
-                    helperText: '请输入你喜欢的类型',
-                  ),
-                  autofocus: false,
-                ),
-                RaisedButton(
-                  onPressed: _choiceAction,
-                  child: Text('选择完毕'),
-                ),
-                Text(
-                  showText,
-                  //兼容性处理，超出1行展示...
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                ),
-              ],
-            ),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('百姓生活+'),
+      ),
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
       ),
     );
-  }
-
-  void _choiceAction() {
-    print('开始选择类型');
-    if (typeController.text.toString() == '') {
-      showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                title: Text('请输入文本'),
-              ));
-    } else {
-      getHttp(typeController.text.toString()).then((val) {
-        setState(() {
-          showText = val['data']['name'].toString();
-        });
-      });
-    }
-  }
-
-  //Future作用就是可以在回调使用then方法
-  Future getHttp(String TypeText) async {
-    try {
-      Response response;
-      var data = {'name': TypeText};
-      response = await Dio().get(
-        'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',
-        queryParameters: data,
-      );
-      return response.data;
-    } catch (e) {
-      return print(e);
-    }
   }
 }
