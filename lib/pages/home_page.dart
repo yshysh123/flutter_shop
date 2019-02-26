630bfce68e07c8e73245e899d575c3b77e9b9113
yusihong1 <yucaihong@jd.com>
2019-02-26 15:43:30 +0800

    :star: 增加swiper效果

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -13,23 +15,57 @@ class _HomePageState extends State<HomePage> {
   String homePageContent = '正在获取数据';
 
   @override
-  void initState() {
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('百姓生活+'),
       ),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            //json数据解释
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDataList: swiper),
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中......'),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+//首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  /*
+   * key 1.0之后可以不写
+   */
+  // SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+  SwiperDiy({this.swiperDataList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network("${swiperDataList[index]['image']}",
+              fit: BoxFit.fill);
+        },
+        //数量
+        itemCount: swiperDataList.length,
+        //导航
+        pagination: new SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
